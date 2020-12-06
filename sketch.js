9e2891a16aa967b0aadd0a04d5daf34f6e7d8ca1
namanagar080 <74126065+namanagar080@users.noreply.github.com>
2020-12-06 17:29:47 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+var bullet,wall,bulletRightEdge,wallLeftEdge;
+var speed,weight,thickness,damage;
+function setup() {
+  createCanvas(1600,400);
+  bullet=createSprite(50, 200, 50, 20);
+  bullet.shapeColor="white";
+  thickness=random(22,83);
+  wall=createSprite(1200,200,thickness,height/2);
+  wall.shapeColor=color(80,80,80);  
+  
+  speed=random(223,321);
+  weight=random(30,52);
+ 
+  car.velocityX=speed;
+}
+
+function draw() {
+  background(255,255,255);
+  if(hasCollided(bullet,wall)){
+    bullet.velocityX=0;
+    damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
+    if(damage<=10){
+     bullet.shapeColor="green";
+    }
+    else{
+      bullet.shapeColor="red";
+    }
+  }
+  drawSprites();
+}
+function hasCollided(lbullet,lwall){
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+  else{
+    return false;
+  }
+}
